410eccfb73b3fbdc7ec00d5e730827b42fecff26
Ivo Sabev <ivolution@gmail.com>
2023-08-19 15:42:25 +0300

    Add mergeTypeDefsAndResolvers function

--- src/GraphQL/index.ts
+++ src/GraphQL/index.ts
@@ -2,3 +2,4 @@ export * from './createFormattedDateResolver';
 export * from './createFormattedDateTimeResolver';
 export * from './fromGlobalId';
 export * from './mapEdgesToArray';
+export * from './mergeTypeDefsAndResolvers';
