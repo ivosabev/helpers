bbcca0494f9def9f7a05a2638ccdf966bf1e960b
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:50:05 +0300

    Add various TS types

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,25 @@ export interface Dictionary<T> {
   [index: string]: T;
 }
 
+export type JSONValue = string | number | boolean | JSONObject | JSONArray;
+
+export interface JSONObject {
+  [x: string]: JSONValue;
+}
+
+export interface JSONArray extends Array<JSONValue> {}
+
+export type OptionalKey<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type Optional<T> = T | undefined | null;
+export type OptionalKeys<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type Unpacked<T> = T extends (infer U)[] ? U : T;
+
+//
+
+export {debounce, isEmpty};
+
 export * from './Array';
 export * from './Date';
 export * from './Dom';
@@ -14,5 +33,3 @@ export * from './Number';
 export * from './Object';
 export * from './Other';
 export * from './String';
-
-export {debounce, isEmpty};
