6121e6d9e09d28110b3d811232b96aa102605e38
Ivo Sabev <ivolution@gmail.com>
2023-08-19 15:42:25 +0300

    Add mergeTypeDefsAndResolvers function

---
+++ src/GraphQL/mergeTypeDefsAndResolvers.ts
@@ -0,0 +1,16 @@
+import merge from 'lodash/merge';
+
+type TypeDefsAndResolvers = {typeDefs: any; resolvers: any};
+
+export const mergeTypeDefsAndResolvers = (typeDefsAndResolvers: TypeDefsAndResolvers[]) => {
+  return typeDefsAndResolvers.reduce(
+    (p, f) => ({
+      resolvers: merge(f.resolvers ? [...p.resolvers, ...(Array.isArray(f.resolvers) ? f.resolvers : [f.resolvers])] : p.resolvers),
+      typeDefs: f.typeDefs ? [...p.typeDefs, ...(Array.isArray(f.typeDefs) ? f.typeDefs : [f.typeDefs])] : p.typeDefs,
+    }),
+    {
+      resolvers: [],
+      typeDefs: [],
+    },
+  );
+};
