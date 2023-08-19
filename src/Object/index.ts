439e0b47834f40798d76092706e7258b62a6bcfe
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:30:47 +0300

    Add isObjectLike function

--- src/Object/index.ts
+++ src/Object/index.ts
@@ -3,5 +3,6 @@ import defaultsDeep from 'lodash/defaultsDeep';
 export {defaultsDeep};
 
 export * from './isPlainObject';
+export * from './isObjectLike';
 export * from './omit';
 export * from './pick';
