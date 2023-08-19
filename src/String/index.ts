56828bc41432cf114d7e7a2754358dac3b0535cc
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:36:15 +0300

    Export pluralize functions in separate files so that in the future we can have our own implementation

--- src/String/index.ts
+++ src/String/index.ts
@@ -1,10 +1,13 @@
 import {upperFirst} from 'lodash';
-import {plural, singular} from 'pluralize';
 
-export {plural, singular, upperFirst};
+export {upperFirst};
 
 export * from './abbr';
 export * from './isEmptyString';
+export * from './isPlural';
+export * from './isSingular';
 export * from './nl2br';
 export * from './pascalCase';
+export * from './plural';
+export * from './singular';
 export * from './slugify';
---
+++ src/String/isPlural.ts
@@ -0,0 +1,1 @@
+export {isPlural} from 'pluralize';
