c5aad0b779e9f83a061bfa6150426bb9022d6e11
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:21:09 +0300

    Cleanup isNumberLike function

--- src/Number/isNumberLike.ts
+++ src/Number/isNumberLike.ts
@@ -1,2 +1,2 @@
 export const isNumberLike = (v: any) =>
-  typeof v === 'number' || (typeof v === 'string' && !Number.isNaN(Number(String(v).trim() || undefined)));
+  (typeof v === 'number' && !Number.isNaN(v)) || (typeof v === 'string' && !Number.isNaN(Number(String(v).trim() || undefined)));
