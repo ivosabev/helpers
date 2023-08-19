71c1e0f51e6e8193def32d827607cd40c6eb8dc1
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:22:33 +0300

    Add toOrdinal function

--- src/Number/index.ts
+++ src/Number/index.ts
@@ -3,6 +3,7 @@ export * from './isNumberLike';
 export * from './random';
 export * from './round';
 export * from './toNumber';
+export * from './toOrdinal';
 export * from './toRadians';
 
 export type NumberLike = number | string;
