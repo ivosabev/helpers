4b6ad2d1a91313df7fe3990416592b0000e7a3e5
Ivo Sabev <ivolution@gmail.com>
2023-08-19 12:21:22 +0300

    Cleanup round function

--- src/Number/round.ts
+++ src/Number/round.ts
@@ -1,3 +1,3 @@
-export const round = (n: number, precision = 2) => {
+export function round(n: number, precision = 2) {
   return +`${Math.round(Number(`${n}e+${precision}`))}e-${precision}`;
-};
+}
