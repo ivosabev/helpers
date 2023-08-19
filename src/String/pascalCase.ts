cd287ba3a4e54ea81eabe216881d57efd18f4b44
Ivo Sabev <ivolution@gmail.com>
2023-08-19 15:41:43 +0300

    Use our local function instead of lodash

--- src/String/pascalCase.ts
+++ src/String/pascalCase.ts
@@ -1,5 +1,5 @@
 import camelCase from 'lodash/camelCase';
-import upperFirst from 'lodash/upperFirst';
+import {upperFirst} from './upperFirst';
 
 export function pascalCase(str: string): string {
   return upperFirst(camelCase(str));
