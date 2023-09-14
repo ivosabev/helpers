a73987761bec97aaf6ddf9a06eac25833017a344
Ivo Sabev <ivolution@gmail.com>
2023-09-14 13:40:07 +0300

    Switch to crypto-js for simplicity

--- src/String/encryptText.ts
+++ src/String/encryptText.ts
@@ -1,17 +1,14 @@
-import crypto from 'node:crypto';
+import CryptoJS from 'crypto-js';
 import {isUndefined} from '../Other/isUndefined';
 
-export function encryptText(text: string, customSecret?: string) {
-  if (isUndefined(customSecret) && isUndefined(process.env.SECRET)) {
+export function encryptText(text: string, secret?: string) {
+  if (isUndefined(secret) && isUndefined(process.env.SECRET)) {
     throw new Error('Environment variable SECRET was not found.');
   }
 
-  const ALGO = 'aes-192-cbc';
-  const KEY = crypto.scryptSync((customSecret ?? process.env.SECRET)!, 'salt', 24);
+  if (isUndefined(secret)) {
+    throw new Error('Secret was not found.');
+  }
 
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(ALGO, KEY, iv);
-  const encrypted = cipher.update(text, 'utf8', 'hex');
-
-  return [encrypted + cipher.final('hex'), Buffer.from(iv).toString('hex')].join('|');
+  return CryptoJS.AES.encrypt(text, secret ?? process.env.SECRET).toString();
 }
