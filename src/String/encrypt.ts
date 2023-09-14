3b88b9faf9a94ceb1df19c45d2490bf8c33e2ae0
Ivo Sabev <ivolution@gmail.com>
2023-09-14 13:43:36 +0300

    Improve encrypt and decrypt functions - Simplify names to encrypt and decrypt - Switch to crypto-js - Allow passing of arrays and objects

--- src/String/encrypt.ts
+++ src/String/encrypt.ts
@@ -1,7 +1,7 @@
 import CryptoJS from 'crypto-js';
 import {isUndefined} from '../Other/isUndefined';
 
-export function encryptText(text: string, secret?: string) {
+export function encrypt(text: string | Record<string, any> | any[], secret?: string) {
   if (isUndefined(secret) && isUndefined(process.env.SECRET)) {
     throw new Error('Environment variable SECRET was not found.');
   }
@@ -10,5 +10,5 @@ export function encryptText(text: string, secret?: string) {
     throw new Error('Secret was not found.');
   }
 
-  return CryptoJS.AES.encrypt(text, secret ?? process.env.SECRET).toString();
+  return CryptoJS.AES.encrypt(JSON.stringify(text), secret ?? process.env.SECRET).toString();
 }
