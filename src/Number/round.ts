caaa4d01f140410150969e80eac4728a7caa6bd4
Ivo Sabev <ivolution@gmail.com>
2023-09-27 11:05:21 +0300

    Add JSDoc to round

--- src/Number/round.ts
+++ src/Number/round.ts
@@ -1,3 +1,10 @@
+/**
+ * Rounds a number to a specified precision.
+ *
+ * @param n - The number to round.
+ * @param [precision=2] - The number of decimal places to round to. *Default: 2*
+ * @returns The rounded number.
+ */
 export function round(n: number, precision = 2) {
   return +`${Math.round(Number(`${n}e+${precision}`))}e-${precision}`;
 }
