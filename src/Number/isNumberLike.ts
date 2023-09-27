2880e5aeb13ba7e525093c622c52538167bafe14
Ivo Sabev <ivolution@gmail.com>
2023-09-27 11:05:31 +0300

    Add TS guard to isNumberLike

--- src/Number/isNumberLike.ts
+++ src/Number/isNumberLike.ts
@@ -1,2 +1,2 @@
-export const isNumberLike = (v: any) =>
+export const isNumberLike = (v: any): v is number | string =>
   (typeof v === 'number' && !Number.isNaN(v)) || (typeof v === 'string' && !Number.isNaN(Number(String(v).trim() || undefined)));
