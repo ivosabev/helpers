7c105a3ef25b4db185fdf955fec8e8cd9aa7c441
Ivo Sabev <ivolution@gmail.com>
2023-10-19 07:50:13 +0300

    Export shuffle from lodash

--- src/Array/index.ts
+++ src/Array/index.ts
@@ -1,4 +1,4 @@
-export {difference, intersection, flatten, times} from 'lodash';
+export {shuffle, difference, intersection, flatten, times} from 'lodash';
 
 export * from './chunk';
 export * from './isEmptyArray';
