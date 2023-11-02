3ce189ba9b30f95ee40f5e6ad690d65979d8cc7a
Ivo Sabev <ivolution@gmail.com>
2023-11-02 17:07:01 +0200

    Do not pass zone to srttotime

--- src/Date/toDateTimeFromRaw.ts
+++ src/Date/toDateTimeFromRaw.ts
@@ -3,7 +3,9 @@ import {DateTime} from 'luxon';
 import {strtotime} from './strtotime';
 
 export function toDateTimeFromRaw(raw: string, options?: DateTimeOptions): DateTime {
-  const zone = (options && options.zone) || null;
-  const timestamp: number = strtotime(raw, zone ? (typeof zone === 'string' ? zone : zone.name) : null);
+  // NOTE: Do not pass zone to strtotime, not sure how it works
+  // but it adjust the resulting timestamp in a weird way and when we pass it to DateTime it gets shifted even more
+  // const zone = (options && options.zone) || null;
+  const timestamp: number = strtotime(raw); // , zone ? (typeof zone === 'string' ? zone : zone.name) : null);
   return timestamp ? DateTime.fromMillis(timestamp, options) : DateTime.invalid('Could not parse successfully the given value.');
 }
