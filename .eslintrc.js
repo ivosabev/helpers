cf07b6a2e22f5896663bc9935c437c0e7d0a46d4
Ivo Sabev <ivolution@gmail.com>
2024-01-05 06:57:22 +0200

    Fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['@ivosabev/eslint-config', '@ivosabev/eslint-config/node'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
 };
