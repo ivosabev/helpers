3847d8f248ef939cc6b4356a88744757f902625a
Ivo Sabev <ivolution@gmail.com>
2024-01-05 11:32:36 +0200

    Add functions that parse FormData to JSON and handle multipart form/data forms

---
+++ src/Other/getFormDataFromRequest.ts
@@ -0,0 +1,34 @@
+import {toJSON} from '../Object/toJSON';
+import type {UploadHandler} from './parseMultipartFormData';
+import {parseMultipartFormData} from './parseMultipartFormData';
+
+// const fromStringToJSON = (v: string) => {
+//   if (
+//     typeof v !== 'object' &&
+//     ((v.indexOf('{') === 0 && v.indexOf('}') === v.length - 1) || (v.indexOf('[') === 0 && v.indexOf(']') === v.length - 1))
+//   ) {
+//     return JSON.parse(v);
+//   }
+//   if (String(v) === 'undefined' || String(v) === 'NaN') {
+//     return undefined;
+//   }
+//   if (String(v) === 'null' || String(v).trim() === '') {
+//     return null;
+//   }
+
+//   return v;
+// };
+
+export async function getFormDataFromRequest(request: Request, uploadHandler: UploadHandler) {
+  if (request.headers.get('content-type')?.includes('multipart/form-data')) {
+    const rawInput = Object.fromEntries(await parseMultipartFormData(request, uploadHandler));
+
+    // TODO: Why is that ?
+    // const input: Record<string, any> = Object.keys(rawInput).reduce((p, c) => ({...p, [c]: fromStringToJSON(rawInput[c])}), {});
+
+    return rawInput;
+  } else {
+    const formData = await request.formData();
+    return toJSON(formData);
+  }
+}
---
+++ src/Other/parseMultipartFormData.ts
@@ -0,0 +1,40 @@
+import {streamMultipart} from '@web3-storage/multipart-parser';
+
+export type UploadHandlerPart = {
+  name: string;
+  filename?: string;
+  contentType: string;
+  data: AsyncIterable<Uint8Array>;
+};
+
+export type UploadHandler = (part: UploadHandlerPart) => Promise<File | string | null | undefined>;
+
+// NOTE: Lifter from remix-server-runtime package, marked as unstable
+export async function parseMultipartFormData(request: Request, uploadHandler: UploadHandler): Promise<FormData> {
+  const contentType = request.headers.get('Content-Type') || '';
+  const [type, boundary] = contentType.split(/\s*;\s*boundary=/);
+
+  if (!request.body || !boundary || type !== 'multipart/form-data') {
+    throw new TypeError('Could not parse content as FormData.');
+  }
+
+  const formData = new FormData();
+  const parts: AsyncIterable<UploadHandlerPart & {done?: true}> = streamMultipart(request.body, boundary);
+
+  for await (const part of parts) {
+    if (part.done) break;
+
+    if (typeof part.filename === 'string') {
+      // only pass basename as the multipart/form-data spec recommends
+      // https://datatracker.ietf.org/doc/html/rfc7578#section-4.2
+      part.filename = part.filename.split(/[/\\]/).pop();
+    }
+
+    const value = await uploadHandler(part);
+    if (typeof value !== 'undefined' && value !== null) {
+      formData.append(part.name, value as any);
+    }
+  }
+
+  return formData;
+}
