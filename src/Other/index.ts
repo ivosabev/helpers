f9d7cff97900b74d534d1ae847db800b338102a4
Ivo Sabev <ivolution@gmail.com>
2024-01-05 06:57:36 +0200

    Add isClient and isServer functions

--- src/Other/index.ts
+++ src/Other/index.ts
@@ -4,10 +4,12 @@ export {orderBy};
 
 export * from './fetchAdvanced';
 export * from './getCityByCityOrZip';
+export * from './isClient';
 export * from './isEqual';
 export * from './isIterable';
 export * from './isNull';
 export * from './isNullOrUndefined';
+export * from './isServer';
 export * from './isUndefined';
 export * from './set';
 export * from './sleep';
