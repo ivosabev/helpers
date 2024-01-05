92df497f4f2a00f52ee45a31ea2c6bb4868afa79
Ivo Sabev <ivolution@gmail.com>
2024-01-05 11:32:36 +0200

    Add functions that parse FormData to JSON and handle multipart form/data forms

--- src/Other/index.ts
+++ src/Other/index.ts
@@ -4,6 +4,7 @@ export {orderBy};
 
 export * from './fetchAdvanced';
 export * from './getCityByCityOrZip';
+export * from './getFormDataFromRequest';
 export * from './isClient';
 export * from './isEqual';
 export * from './isIterable';
@@ -11,5 +12,6 @@ export * from './isNull';
 export * from './isNullOrUndefined';
 export * from './isServer';
 export * from './isUndefined';
+export * from './parseMultipartFormData';
 export * from './set';
 export * from './sleep';
