412594ea37cb6f0409c5546bb0d192e3f3e8e2cd
Ivo Sabev <ivolution@gmail.com>
2024-01-06 08:53:09 +0200

    Handle JSON and Array being sent in stringified form into a FormData input

--- src/Other/getFormDataFromRequest.ts
+++ src/Other/getFormDataFromRequest.ts
@@ -2,31 +2,30 @@ import {toJSON} from '../Object/toJSON';
 import type {UploadHandler} from './parseMultipartFormData';
 import {parseMultipartFormData} from './parseMultipartFormData';
 
-// const fromStringToJSON = (v: string) => {
-//   if (
-//     typeof v !== 'object' &&
-//     ((v.indexOf('{') === 0 && v.indexOf('}') === v.length - 1) || (v.indexOf('[') === 0 && v.indexOf(']') === v.length - 1))
-//   ) {
-//     return JSON.parse(v);
-//   }
-//   if (String(v) === 'undefined' || String(v) === 'NaN') {
-//     return undefined;
-//   }
-//   if (String(v) === 'null' || String(v).trim() === '') {
-//     return null;
-//   }
+const fromStringToJSON = (v: FormDataEntryValue) => {
+  if (!(v instanceof File)) {
+    if (
+      typeof v !== 'object' &&
+      ((v.indexOf('{') === 0 && v.indexOf('}') === v.length - 1) || (v.indexOf('[') === 0 && v.indexOf(']') === v.length - 1))
+    ) {
+      return JSON.parse(v);
+    }
+    if (String(v) === 'undefined' || String(v) === 'NaN') {
+      return undefined;
+    }
+    if (String(v) === 'null' || String(v).trim() === '') {
+      return null;
+    }
+  }
 
-//   return v;
-// };
+  return v;
+};
 
 export async function getFormDataFromRequest(request: Request, uploadHandler: UploadHandler) {
   if (request.headers.get('content-type')?.includes('multipart/form-data')) {
     const rawInput = Object.fromEntries(await parseMultipartFormData(request, uploadHandler));
-
-    // TODO: Why is that ?
-    // const input: Record<string, any> = Object.keys(rawInput).reduce((p, c) => ({...p, [c]: fromStringToJSON(rawInput[c])}), {});
-
-    return rawInput;
+    const input: Record<string, any> = Object.keys(rawInput).reduce((p, c) => ({...p, [c]: fromStringToJSON(rawInput[c])}), {});
+    return input;
   } else {
     const formData = await request.formData();
     return toJSON(formData);
