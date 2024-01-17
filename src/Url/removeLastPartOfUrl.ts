3035d521650d417def60d3f4b28e8b265c05a546
Ivo Sabev <ivolution@gmail.com>
2024-01-17 10:48:09 +0200

    Fix removeLastPartOfUrl to work even when the url ends with /

--- src/Url/removeLastPartOfUrl.ts
+++ src/Url/removeLastPartOfUrl.ts
@@ -1,5 +1,9 @@
 export const removeLastPartOfUrl = (url: string) => {
   const parts = url.split('/');
-  parts.pop();
+  const removedPart = parts.pop();
+  // If url is ending with a slash, we need to pop again
+  if (removedPart === '') {
+    parts.pop();
+  }
   return parts.join('/');
 };
