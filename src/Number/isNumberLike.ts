c62ee999265d55ba85c097e99e43ca8c549714d8
Ivo Sabev <ivolution@gmail.com>
2024-02-18 12:34:31 +0200

    Handle bigint in isNumberLike

--- src/Number/isNumberLike.ts
+++ src/Number/isNumberLike.ts
@@ -1,2 +1,4 @@
 export const isNumberLike = (v: any): v is number | string =>
-  (typeof v === 'number' && !Number.isNaN(v)) || (typeof v === 'string' && !Number.isNaN(Number(String(v).trim() || undefined)));
+  (typeof v === 'number' && !Number.isNaN(v)) ||
+  (typeof v === 'bigint' && !Number.isNaN(Number(v))) ||
+  (typeof v === 'string' && !Number.isNaN(Number(String(v).trim() || undefined)));
