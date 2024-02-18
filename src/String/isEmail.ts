410b9d25146b801fe5eaf18f678cde8eb263bb1a
Ivo Sabev <ivolution@gmail.com>
2024-02-18 14:31:50 +0200

    Add typeguard to isEmail

--- src/String/isEmail.ts
+++ src/String/isEmail.ts
@@ -1,5 +1,5 @@
-export function isEmail(email: string) {
+export function isEmail(v: string): v is string {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // eslint-disable-line max-len
-  return re.test(email);
+  return re.test(v);
 }
