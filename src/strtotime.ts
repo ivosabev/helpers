c29a15c4ca799be2100f23ed584cfed70ce6c56e
Ivo Sabev <ivolution@gmail.com>
2024-08-13 11:47:38 +0300

    Fix wrong path to the timezones file

--- src/strtotime.ts
+++ src/strtotime.ts
@@ -4,7 +4,7 @@
 // This is modified version of kvz/locutus strtotime with support for timezones and unix timestamps
 // Source: https://github.com/kvz/locutus/blob/strtotime-patch-1/src/php/datetime/strtotime.js
 
-import {timezones} from './Date/timezones';
+import {timezones} from './timezones.js';
 
 const reSpace = '[ \\t]+';
 const reSpaceOpt = '[ \\t]*';
