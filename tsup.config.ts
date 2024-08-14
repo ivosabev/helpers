cae89929915e2ce1d131ad9b3fa95ac1e95a3fc2
Ivo Sabev <ivolution@gmail.com>
2024-08-14 13:14:16 +0300

    Emit CJS files

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ export default defineConfig((options) => {
     dts: true,
     entry: ['src/**/*.(ts|tsx)'],
     external: ['fs', 'path', 'stream', 'node:fs', 'node:path', 'node:stream'],
-    format: ['esm'],
+    format: ['cjs', 'esm'],
     // minify: !options.watch,
     outDir: 'dist',
     sourcemap: true,
