12fe36ce5de6cec9b59e0f4003db07e9585e0d62
Ivo Sabev <ivolution@gmail.com>
2024-08-21 15:41:57 +0300

    Add missing paginatedConnectionFromArray function

---
+++ src/paginatedConnectionFromArray.ts
@@ -0,0 +1,34 @@
+import {connectionFromArray} from 'graphql-relay';
+
+export const paginatedConnectionFromArray = async (records: any[], total: number, rawArgs: any) => {
+  // Clean values
+  const DEFAULT_LIMIT = 50;
+  const args = {
+    ...rawArgs,
+    limit: rawArgs.limit || DEFAULT_LIMIT,
+    offset: rawArgs.limit ? (rawArgs.page ? (rawArgs.page - 1) * rawArgs.limit : rawArgs.offset || undefined) : undefined,
+    page: rawArgs.page || undefined,
+  };
+
+  // Get edges
+  const connection = connectionFromArray(records, args);
+
+  // Get pageInfo
+  const pages = args.limit ? Math.ceil(total / args.limit) : 1;
+  const page = args.limit ? (args.offset || 0) / args.limit + 1 : 1;
+  const start = total ? (args.offset || 0) + 1 : 0;
+  const end = page * args.limit < total ? page * args.limit : total;
+  const hasNext = page < pages;
+  const hasPrev = page > 1;
+
+  return {
+    ...connection,
+    end,
+    hasNext,
+    hasPrev,
+    page,
+    pages,
+    start,
+    total,
+  };
+};
--- src/paginatedConnectionFromPromisedArray.ts
+++ src/paginatedConnectionFromPromisedArray.ts
@@ -1,7 +1,7 @@
 import type {GraphQLResolveInfo} from 'graphql';
 import {GraphQLBoolean, GraphQLInt, GraphQLNonNull} from 'graphql';
-import {connectionFromArray} from 'graphql-relay';
 import {omit} from './omit.js';
+import {paginatedConnectionFromArray} from './paginatedConnectionFromArray.js';
 
 export const paginationArgs = {
   limit: {
@@ -68,37 +68,4 @@ export const paginatedConnectionFromPromisedArray = async (fn: any, rawArgs: any
     console.log('Error', e);
   }
   return [];
-};
-
-export const paginatedConnectionFromArray = async (records: any[], total: number, rawArgs: any) => {
-  // Clean values
-  const DEFAULT_LIMIT = 50;
-  const args = {
-    ...rawArgs,
-    limit: rawArgs.limit || DEFAULT_LIMIT,
-    offset: rawArgs.limit ? (rawArgs.page ? (rawArgs.page - 1) * rawArgs.limit : rawArgs.offset || undefined) : undefined,
-    page: rawArgs.page || undefined,
-  };
-
-  // Get edges
-  const connection = connectionFromArray(records, args);
-
-  // Get pageInfo
-  const pages = args.limit ? Math.ceil(total / args.limit) : 1;
-  const page = args.limit ? (args.offset || 0) / args.limit + 1 : 1;
-  const start = total ? (args.offset || 0) + 1 : 0;
-  const end = page * args.limit < total ? page * args.limit : total;
-  const hasNext = page < pages;
-  const hasPrev = page > 1;
-
-  return {
-    ...connection,
-    end,
-    hasNext,
-    hasPrev,
-    page,
-    pages,
-    start,
-    total,
-  };
 };
