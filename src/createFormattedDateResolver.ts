dab3dc3eb5377e7658279fc06fd6771caa91f878
Ivo Sabev <ivolution@gmail.com>
2024-11-11 17:47:00 +0200

    Fix graphql date resolver helper functions not working

--- src/createFormattedDateResolver.ts
+++ src/createFormattedDateResolver.ts
@@ -1,9 +1,21 @@
 import {DateTime} from 'luxon';
 
+// TODO: This needs to accept date only and still work, like '2021-01-01', instead of full date time '2021-01-01T00:00:00.000Z'
+
 export function createFormattedDateResolver(field: string, defaultFormat: string = 'y-MM-dd') {
   return async function (obj: Record<string, any>, args: any) {
-    return DateTime.fromMillis(Date.parse(obj[field]))
-      .toUTC()
-      .toFormat(args.format || defaultFormat);
+    const date = DateTime.fromMillis(Date.parse(obj[field]));
+
+    if (!date.isValid) {
+      throw new Error(`Field ${field} is not a valid date`);
+    }
+
+    if (args.format) {
+      return date.toUTC().toFormat(args.format);
+    } else if (defaultFormat) {
+      return date.toUTC().toFormat(defaultFormat);
+    } else {
+      return date.toUTC().toISODate();
+    }
   };
 }
--- src/createFormattedDateTimeResolver.ts
+++ src/createFormattedDateTimeResolver.ts
@@ -1,9 +1,27 @@
 import {DateTime} from 'luxon';
 
+/**
+ * Helper function to create a resolver for a formatted date time field.
+ *
+ * GraphQL example:
+ *
+ * `deliveryEndAt(format: String): DateTime!`
+ *
+ * @param field
+ * @param defaultFormat
+ * @returns
+ */
 export function createFormattedDateTimeResolver(field: string, defaultFormat?: string) {
-  return async function (obj: Record<string, any>, _args: any) {
+  return async function (obj: Record<string, any>, args: {format?: string}) {
     const date = DateTime.fromMillis(Date.parse(obj[field]));
-    if (defaultFormat) {
+
+    if (!date.isValid) {
+      throw new Error(`Field ${field} is not a valid date`);
+    }
+
+    if (args.format) {
+      return date.toUTC().toFormat(args.format);
+    } else if (defaultFormat) {
       return date.toUTC().toFormat(defaultFormat);
     } else {
       return date.toUTC().toISO();
