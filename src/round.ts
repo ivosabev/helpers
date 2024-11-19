2a12abfb70628037561657e9936ac4e58e930cfc
Ivo Sabev <ivolution@gmail.com>
2024-11-19 14:14:46 +0200

    Fix the correctness of the rounding function

--- src/round.ts
+++ src/round.ts
@@ -1,10 +1,17 @@
 /**
  * Rounds a number to a specified precision.
  *
+ * @example
+ * // returns 999.99
+ * round(285.71*3.5);
+ *
  * @param n - The number to round.
- * @param [precision=2] - The number of decimal places to round to. *Default: 2*
+ * @param [precision=2] - The decimal places to round to. *Default: 2*
  * @returns The rounded number.
+ *
  */
-export function round(n: number, precision = 2) {
-  return +`${Math.round(Number(`${n}e+${precision}`))}e-${precision}`;
+export function round(value: number, precision: number = 2) {
+  const sign = value < 0 ? -1 : 1;
+  const factor = 10 ** precision;
+  return sign * Math.round(Math.abs(value * factor + 0.00000001)) / factor;
 }
