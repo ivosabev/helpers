0458ee2c18ad693896ac92d23cc2fbfeb9900699
Ivo Sabev <ivolution@gmail.com>
2024-11-27 16:06:34 +0200

    Remove empty strings inside numbers in toNumber

--- src/toNumber.ts
+++ src/toNumber.ts
@@ -3,5 +3,6 @@ import {isNumberLike} from './isNumberLike.js';
 export function toNumber(v: any, d?: number): number;
 export function toNumber(v: any, d?: undefined): number | undefined;
 export function toNumber(v: any, d?: number | undefined) {
-  return isNumberLike(v) ? Number(v) : d;
+  const vv = String(v).replace(/\s+/g, ' ');
+  return isNumberLike(vv) ? Number(vv) : d;
 }
