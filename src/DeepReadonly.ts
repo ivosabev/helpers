f8b100f9fd2f0caf5875ed69128aa9631897aeef
Ivo Sabev <ivolution@gmail.com>
2024-12-11 13:07:36 +0200

    Add DeepReadonly utility

---
+++ src/DeepReadonly.ts
@@ -0,0 +1,14 @@
+export type DeepReadonly<T> =
+  T extends Primitive ? T :
+  T extends Array<infer U> ? DeepReadonlyArray<U> :
+  DeepReadonlyObject<T>
+
+type Primitive =
+  string | number | boolean | undefined | null
+
+interface DeepReadonlyArray<T>
+  extends ReadonlyArray<DeepReadonly<T>> {}
+
+type DeepReadonlyObject<T> = {
+  readonly [P in keyof T]: DeepReadonly<T[P]>
+}
