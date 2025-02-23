706f432ccc4faf243a2201d9326a186e38c03948
Ivo Sabev <ivolution@gmail.com>
2025-02-23 11:34:31 +0200

    toNumber should return NaN, except when default argument is given, even if that default argument is undefined

--- src/toNumber.ts
+++ src/toNumber.ts
@@ -1,8 +1,9 @@
-import {isNumberLike} from './isNumberLike.js';
-
-export function toNumber(v: any, d?: number): number;
-export function toNumber(v: any, d?: undefined): number | undefined;
-export function toNumber(v: any, d?: number | undefined) {
-  const vv = String(v).replace(/\s+/g, ' ');
-  return isNumberLike(vv) ? Number(vv) : d;
+export function toNumber(v: unknown): number;
+export function toNumber(v: unknown, d?: number): number;
+export function toNumber(v: unknown, d?: number | undefined): number | undefined;
+export function toNumber(v: unknown, d?: number | null): number | null;
+export function toNumber(v: unknown, d?: number | null | undefined): number | null | undefined {
+  const vv = Number(v);
+  const dd = arguments.length === 1 ? NaN : d;
+  return Number.isNaN(vv) ? dd : vv;
 }
