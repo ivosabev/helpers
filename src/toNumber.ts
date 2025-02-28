ecac66c749ca9c89f2992d2fd959fba5506586df
Ivo Sabev <ivolution@gmail.com>
2025-02-28 16:06:36 +0200

    Fix toNumber returning 0 when null is passed

--- src/toNumber.ts
+++ src/toNumber.ts
@@ -1,9 +1,12 @@
+import {isNumberLike} from './isNumberLike.js';
+
 export function toNumber(v: unknown): number;
 export function toNumber(v: unknown, d?: number): number;
 export function toNumber(v: unknown, d?: number | undefined): number | undefined;
 export function toNumber(v: unknown, d?: number | null): number | null;
 export function toNumber(v: unknown, d?: number | null | undefined): number | null | undefined {
-  const vv = Number(v);
   const dd = arguments.length === 1 ? NaN : d;
+  // isNumberLike checks for null, undefined and other, which if sent to Number will return 0, which is not what we want
+  const vv = isNumberLike(v) ? Number(v) : dd;
   return Number.isNaN(vv) ? dd : vv;
 }
