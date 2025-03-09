a16e02dbf01f9484662bf3454b2e1f9f2328160f
Ivo Sabev <ivolution@gmail.com>
2025-03-09 11:13:31 +0200

    Allow string to be passed to toOrdinal and cast it

--- src/toOrdinal.ts
+++ src/toOrdinal.ts
@@ -1,6 +1,8 @@
-export function toOrdinal(n: number) {
+import {toNumber} from './toNumber.js';
+
+export function toOrdinal(n: number | string) {
   const s = ['th', 'st', 'nd', 'rd'];
-  const v = n % 100;
+  const v = toNumber(n) % 100;
   // @ts-expect-error
   return n + (s[(v - 20) % 10] || s[v] || s[0]);
 }
