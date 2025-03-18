5dc99c7d8c812942f4b48120bbaf70164df6d3e7
Ivo Sabev <ivolution@gmail.com>
2025-03-18 14:17:00 +0200

    Add retry functionality to fetchAdvanced

--- src/fetchAdvanced.ts
+++ src/fetchAdvanced.ts
@@ -1,16 +1,24 @@
-type RequestInitAdvanced = RequestInit & {timeout: number};
+type RequestInitAdvanced = RequestInit & {retry?: number, timeout?: number};
 
-export async function fetchAdvanced(resource: RequestInfo, options: RequestInitAdvanced = {timeout: 10000}) {
-  const {timeout = 10000} = options;
+export async function fetchAdvanced(resource: RequestInfo, options?: RequestInitAdvanced) {
+  const retry = Number((options?.retry ?? 0) < 1 ? 1 : options?.retry);
+  const timeout = Number((options?.timeout ?? 10000) < 1000 ? 1000 : options?.timeout);
 
-  const controller = new AbortController();
-  const id = setTimeout(() => controller.abort(), timeout);
+  for (let attempt = 1; attempt <= retry ; attempt++) {
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
 
-  const response = await fetch(resource, {
-    ...options,
-    signal: controller.signal,
-  });
-  clearTimeout(id);
+    try {
+      const response = await fetch(resource, {...options, signal: controller.signal});
+      return response;
+    } catch (e: any) {
+      if (attempt === retry || e.name === 'AbortError') {
+        throw e;
+      }
+    } finally {
+      clearTimeout(id);
+    }
+  }
 
-  return response;
+  throw new Error('Unknown error');
 }
