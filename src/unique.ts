b186dda87b78974e4f463d603630bae5cb7d659b
Ivo Sabev <ivolution@gmail.com>
2025-06-16 09:07:59 +0300

    Improve types

--- src/unique.ts
+++ src/unique.ts
@@ -2,8 +2,8 @@ import {type Dictionary} from './Dictionary.js';
 
 export function unique(arr: number[]): number[];
 export function unique(arr: string[]): string[];
-export function unique(arr: Dictionary<string>[], property: string): Dictionary<string>[];
-export function unique(arr: any[], property?: string) {
+export function unique<T extends Dictionary<any>>(arr: T[], property: string): T[];
+export function unique(arr: any[], property?: string): any[] {
   if (property) {
     // TODO: This may not be working properly, needs to be tested
     return arr.filter((o, p, a) => a.map((o2) => o2[property]).indexOf(o[property]) === p);
