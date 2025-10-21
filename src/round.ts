fa9b48219db4f874e0d1e31dcf24427420aa32eb
Ivo Sabev <ivolution@gmail.com>
2025-10-21 16:44:30 +0300

    Fix -0 to be 0

--- src/round.ts
+++ src/round.ts
@@ -7,11 +7,13 @@
  *
  * @param n - The number to round.
  * @param [precision=2] - The decimal places to round to. *Default: 2*
+ *
  * @returns The rounded number.
  *
  */
 export function round(value: number, precision: number = 2) {
   const sign = value < 0 ? -1 : 1;
   const factor = 10 ** precision;
-  return sign * Math.round(Math.abs(value * factor + 0.00000001)) / factor;
+  const result = sign * Math.round(Math.abs(value * factor + 0.00000001)) / factor;
+  return result === -0 ? 0 : result;
 }
