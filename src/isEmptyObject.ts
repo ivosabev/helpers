7a05303d962ce4e19aaf7ab1f4e89111a0585e97
Ivo Sabev <ivolution@gmail.com>
2026-06-07 10:01:59 +0300

    Improve the isEmptyObject function

--- src/isEmptyObject.ts
+++ src/isEmptyObject.ts
@@ -1,6 +1,8 @@
-export const isEmptyObject = (obj: Record<string, any>): obj is {} => {
-  for (const _i in obj) {
-    return false;
+export const isEmptyObject = (obj: Record<string, unknown>): obj is Record<string, never> => {
+  for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return false;
+    }
   }
   return true;
 };
